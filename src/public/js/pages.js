7ffa6258dcc4b8757e19371492b3e9a40b38ec97
Millena Santos da Costa <millena.costa@bradesco.com.br>
2023-10-17 21:35:51 -0300

    Configurei formulario de busca e botao mostrar todos

---
+++ src/public/js/pages.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+const unorm = require('unorm');
+
+function pageIndex(req,res) {
+    return res.render("index.html")
+}
+
+function pageProductsList(req,res) {
+    const filePath = "src/produtos.json";
+
+    // let products = readFileSync(filePath, "utf8");
+    let products = fs.readFileSync(filePath, "utf8");
+    if(products) {
+        products = JSON.parse(products)
+    }
+    let listaProdutos = products.products;
+    
+    return res.render("lista-presentes.html", { listaProdutos })
+}
+
+function removeAcentos(string1) {
+    string1 = unorm.nfd(string1).replace(/[\u0300-\u036f]/g, '');
+    return string1;
+}
+
+function findProductByName(req, res) {
+
+    const mostrarTodos = req.body.todos;
+
+    if(!mostrarTodos || mostrarTodos == undefined) {
+        const nomeProduto = req.body.product;
+        const filePath = "src/produtos.json";
+    
+        let products = fs.readFileSync(filePath, "utf8");
+        if(products) {
+            products = JSON.parse(products)
+        }
+        let actualListProducts = Array.from(products.products);
+        let listaProdutos = [];
+        let produtoAtual = "";
+        let produtoPedido = ""
+        for(let i = 0; i < actualListProducts.length; i++) {
+            produtoAtual = removeAcentos(actualListProducts[i].name.toLowerCase());
+            produtoPedido = removeAcentos(nomeProduto.toLowerCase());
+            if (produtoAtual.includes(produtoPedido)) {
+                listaProdutos.push(actualListProducts[i]);
+            }
+        }
+        return res.render("lista-presentes.html", { listaProdutos })
+    }
+    return res.redirect("/lista-presentes")
+}
+
+
+module.exports = {
+    pageIndex,
+    pageProductsList,
+    findProductByName
+}
--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 //servidor
-const express = require('express')
-const server = express()
-const { pageIndex, pageProductsList} = require("./pages")
+const express = require("express")
+const { pageIndex, pageProductsList, findProductByName} = require("./public/js/pages.js")
 
 // configurar nunjucks (template engine)
-const nunjucks = require('nunjucks')
+const nunjucks = require("nunjucks");
+const server = express()
 nunjucks.configure('src/views', {
     express: server,
     noCache: true,
@@ -14,10 +14,15 @@ server
 //receber os dados do req.body
 .use(express.urlencoded({ extended: true }))
 // configurar arquivos estáticos (css,scripts,imagens)
-.use(express.static("public")) //comecar enxergar o public
+// .use(express.static("public")) //comecar enxergar o public
+.use(express.static(__dirname + "/public")) //comecar enxergar o public
 
 //rotas da aplicação
 .get("/", pageIndex)
 .get("/lista-presentes", pageProductsList)
+.post("/busca-produtos", findProductByName)
+.get("/busca-produtos", (req, res) => {
+    res.redirect("/lista-presentes")
+})
 
 .listen(4000)
